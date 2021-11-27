7e11db615f352be9697655800e9933ec4fe68e1a
Jen <33593694+acerf129@users.noreply.github.com>
2021-11-27 17:30:56 +0800

    Upload test files

---
+++ Script/main.js
@@ -0,0 +1,31 @@
+const myHeading = document.querySelector('h1');
+let myButton=document.querySelector('button');
+myHeading.textContent="Good Morning Britain";
+
+const myImage = document.querySelector('img[alt="Corgi"]');
+myImage.onclick=function(){
+    let mySrc = myImage.getAttribute('src');
+    //console.log(mySrc);
+    if(mySrc==="Image/Corgi.jpeg"){
+        alert("It's Corgi");
+    }
+}
+
+if(!localStorage.getItem('name')){
+    setUserName();
+}else{
+    let storeName=localStorage.getItem('name');
+    myHeading.textContent="Good Morning Britain, "+storeName;
+}
+function setUserName(){
+    let name =prompt("Please enter your name.");
+    if(!name){
+        setUserName();
+        return;
+    }
+    localStorage.setItem('name',name);
+    myHeading.textContent="Good Morning Britain, "+name;
+}
+myButton.onclick=function(){
+    setUserName();
+}
