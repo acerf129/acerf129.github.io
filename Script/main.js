beba2a33c82fe7099931b3d4c86881422da4fc4d
Jen <33593694+acerf129@users.noreply.github.com>
2022-03-15 21:00:17 +0800

    update blog

--- Script/main.js
+++ Script/main.js
@@ -1,31 +1,14 @@
-const myHeading = document.querySelector('h1');
-let myButton=document.querySelector('button');
-myHeading.textContent="Good Morning ";
-
-const myImage = document.querySelector('img[alt="Corgi"]');
-myImage.onclick=function(){
-    let mySrc = myImage.getAttribute('src');
-    //console.log(mySrc);
-    if(mySrc==="Image/Corgi.jpeg"){
-        alert("It's Corgi");
+var mydata = JSON.parse(data);
+if (mydata!=null){
+    let mydataString="";
+    for(var i=0;i<mydata.length;i++){
+        mydataString+='<div class="card"><a href="'+mydata[i].url_link+'"><h2>'+mydata[i].title+'</h2></a>'+
+        '<h3>'+mydata[i].body+'</h3>'+
+        '<p>'+mydata[i].dataCreate+'</p></div>'
     }
+    const rightcolumn= document.querySelector('.rightcolumn');
+    rightcolumn.insertAdjacentHTML( 'beforeend', mydataString );
+    const item_count = document.querySelector('.item-count');
+    item_count.insertAdjacentHTML( 'beforeend', mydata.length);
 }
-
-if(!localStorage.getItem('name')){
-    setUserName();
-}else{
-    let storeName=localStorage.getItem('name');
-    myHeading.textContent="Good Morning , "+storeName;
-}
-function setUserName(){
-    let name =prompt("Please enter your name.");
-    if(!name){
-        setUserName();
-        return;
-    }
-    localStorage.setItem('name',name);
-    myHeading.textContent="Good Morning , "+name;
-}
-myButton.onclick=function(){
-    setUserName();
-}
+        
