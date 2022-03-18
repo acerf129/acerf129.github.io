db4e2b5baa32d01d9b892239b87264d81ebda213
Jen <33593694+acerf129@users.noreply.github.com>
2022-03-18 11:13:40 +0800

    Update Index

--- Script/main.js
+++ Script/main.js
@@ -2,9 +2,10 @@ var mydata = JSON.parse(data);
 if (mydata!=null){
     let mydataString="";
     for(var i=0;i<mydata.length;i++){
-        mydataString+='<div class="card"><a href="'+mydata[i].url_link+'"><h2>'+mydata[i].title+'</h2></a>'+
+        mydataString+='<div class="card"><a href="'+mydata[i].url_link+'" target="_blank"><h2>'+mydata[i].title+'</h2></a>'+
+        '<h5>'+mydata[i].dataCreate+' by Mike'+'</h5>'+
         '<h3>'+mydata[i].body+'</h3>'+
-        '<p>'+mydata[i].dataCreate+'</p></div>'
+        '</div>'
     }
     const rightcolumn= document.querySelector('.rightcolumn');
     rightcolumn.insertAdjacentHTML( 'beforeend', mydataString );
