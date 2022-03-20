2f56a3d33a9b7c261e9cd0a273ea3f2c1ecf88fa
Jen <33593694+acerf129@users.noreply.github.com>
2022-03-20 15:07:18 +0800

    update blog

--- Script/main.js
+++ Script/main.js
@@ -5,6 +5,7 @@ if (mydata!=null){
         mydataString+='<div class="card"><a href="'+mydata[i].url_link+'" target="_blank"><h2>'+mydata[i].title+'</h2></a>'+
         '<h5>'+mydata[i].dataCreate+' by Mike'+'</h5>'+
         '<h3>'+mydata[i].body+'</h3>'+
+        '<a href="'+mydata[i].url_link+'" target="_blank"><h4>Continue Reading ....</h4></a>'+
         '</div>'
     }
     const rightcolumn= document.querySelector('.rightcolumn');
